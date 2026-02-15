4ec91ed2792e83415294109b1f9755a4d8c7c616
jamesschuler <jameshschuler1@gmail.com>
2026-02-15 15:56:57 -0800

    feat: fixed dialog scroll

--- src/components/BulkEditDialog.tsx
+++ src/components/BulkEditDialog.tsx
@@ -24,12 +24,12 @@ export const BulkEditDialog = ({
           <ListOrdered className="w-5 h-5" />
         </Button>
       </DialogTrigger>
-      <DialogContent className="sm:max-w-[425px] max-h-[80vh] flex flex-col">
-        <DialogHeader>
+      <DialogContent className="sm:max-max-w-[425px] h-[80vh] flex flex-col p-0">
+        <DialogHeader className="p-6 pb-0">
           <DialogTitle>Edit All Goals</DialogTitle>
         </DialogHeader>
-        <ScrollArea className="flex-1 pr-4">
-          <div className="space-y-3 py-4">
+        <ScrollArea className="flex-1 w-full px-6">
+          <div className="space-y-3 py-6">
             {goals.map((g, i) => (
               <div key={i} className="flex items-center gap-3">
                 <span className="text-xs font-mono opacity-50 w-4">
@@ -38,7 +38,7 @@ export const BulkEditDialog = ({
                 <Input
                   value={g.goal}
                   onChange={(e) => onUpdate(i, e.target.value)}
-                  className="h-8 text-sm"
+                  className="h-9 text-sm"
                 />
               </div>
             ))}
